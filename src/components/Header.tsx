ae6a0201d9288713f9d5051a83313f35b915103d
iplowman <isaac.plowman2005@gmail.com>
2025-06-26 14:56:19 -0400

    Scaling, font, and color updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,19 +5,21 @@ import './Header.css';
 export default function Header() {
   return (
     <header className='header'>
-      <div className='logoContainer'>
-        <img src={logo} alt="Hearts in Action logo" className='logo' />
-        <Link to="/" className="link" style={{ fontSize: '1.5rem' }}>
-          Hearts in Action
-        </Link>
-      </div>
-      <nav className='nav'>
-        <NavLink to="/" className="nav-link">Home</NavLink>
-        <NavLink to="/about" className="nav-link">About</NavLink>
-        <NavLink to="/donate" className="nav-link">Donate</NavLink>
-        <NavLink to="/team" className="nav-link">Team</NavLink>
-        <NavLink to="/ai" className="nav-link">AI Census</NavLink>
-      </nav>
+      <div className='header-content'>
+         <div className='logoContainer'>
+          <img src={logo} alt="Hearts in Action logo" className='logo' />
+          <Link to="/" className="link" style={{ fontSize: '1.5rem' }}>
+            Hearts in Action
+          </Link>
+        </div>
+        <nav className='nav'>
+          <NavLink to="/" className="nav-link">Home</NavLink>
+          <NavLink to="/about" className="nav-link">About</NavLink>
+          <NavLink to="/donate" className="nav-link">Donate</NavLink>
+          <NavLink to="/team" className="nav-link">Team</NavLink>
+          <NavLink to="/ai" className="nav-link">AI Census</NavLink>
+        </nav>
+      </div>  
     </header>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,109 +7,113 @@ import { NavLink } from 'react-router-dom';
 function Home() {
     return ( 
         <>
-        {/*Hero Section*/}
-        <section className='gradient-section'>
-            <div className='grid'>
-                <div style={{maxWidth: '600px', fontFamily: 'sans-serif'}}>
-                    <h1 className='home-h1' style={{textAlign: 'left', fontSize: '36px'}}>Fighting Food Insecurity at{" "}
-                    <span style={{color: '#f08080'}}>UMD</span>
-                    </h1>
-                    <p style={{fontSize: '24px', textAlign: 'left', color: 'var(--gray-primary)', borderBottom: '0.1rem solid white', paddingBottom: '4rem'}}>
-                    Over 27% of UMD students face food insecurity. That's 1 in 4 of our peers 
-                    struggling to afford their next meal. Join Hearts in Action to break this cycle.
+        <div className='home-content'>
+            {/*Hero Section*/}
+            <section className='gradient-section'>
+                <div className='container'>
+                    <div className='grid'>
+                        <div style={{maxWidth: '40rem'}}>
+                            <h1 className='home-h1' style={{textAlign: 'left', fontSize: '2.25rem'}}>Fighting Food Insecurity at{" "}
+                            <span style={{color: '#f08080'}}>UMD</span>
+                            </h1>
+                            <p style={{fontSize: '1.5rem', textAlign: 'left', color: 'var(--gray-primary)', borderBottom: '0.1rem solid white', paddingBottom: '4rem'}}>
+                            Over 27% of UMD students face food insecurity. That's 1 in 4 of our peers 
+                            struggling to afford their next meal. Join Hearts in Action to break this cycle.
+                            </p>
+                        </div>
+                        <div style={{position: 'relative'}}>
+                            <img src={image_7} alt="image_7" style={{borderRadius: '0.5rem', border: '0.1rem solid white', width: '40rem'}} />
+                            <div className="caption">
+                                <span style={{fontSize: '1.5rem', color: '#f08080'}}><b>27%</b></span>
+                                <div>of UMD students face food insecurity</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/*Statistics*/}
+            <section className='stat-background'>
+                <div className='header-layout'>
+                    <h1 className='home-h1'>The Reality of Food Insecurity</h1>
+                    <p className='header-description' style={{color: 'var(--gray-primary)'}}>These aren't just numbers—they represent real students in our community who need our support.</p>
+                </div>
+                <div className='card-layout'>
+                    <div className='card' style={{backgroundColor: 'rgba(196, 0, 0, 0.2)'}}>
+                        <div className='card-title'>40%</div>
+                        <div className='card-desc'>Fall asleep in class or can't focus due to hunger</div>
+                    </div>
+                    <div className='card' style={{backgroundColor: 'rgba(170, 111, 0, 0.2)'}}>
+                        <div className='card-title'>13%</div>
+                        <div className='card-desc'>Have failed assignments because they didn't have enough to eat</div>
+                    </div>
+                    <div className='card' style={{backgroundColor: 'rgba(179, 90, 90, 0.2)'}}>
+                        <div className='card-title'>25%</div>
+                        <div className='card-desc'>Students struggle to afford their next meal</div>
+                    </div>
+                </div>
+            </section>
+            {/*How We Help*/}
+            <section className='help-background'>
+                <div className='header-layout'>
+                    <h1 className='home-h1'>How We Help</h1>
+                    <p className='header-description' style={{color: 'var(--gray-primary)'}}>
+                        Hearts in Action provides comprehensive support to address food and material insecurity 
+                        in our community through various initiatives.
                     </p>
                 </div>
-                <div style={{position: 'relative'}}>
-                    <img src={image_7} alt="image_7" style={{borderRadius: '0.5rem', border: '0.1rem solid white'}} />
-                    <div className="caption">
-                        <span style={{fontSize: '1.5rem', color: '#f08080'}}><b>27%</b></span>
-                        <div>of UMD students face food insecurity</div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        {/*Statistics*/}
-        <section className='stat-background'>
-            <div className='header-layout'>
-                <h1 className='home-h1'>The Reality of Food Insecurity</h1>
-                <p className='header-description' style={{color: 'var(--gray-primary)'}}>These aren't just numbers—they represent real students in our community who need our support.</p>
-            </div>
-            <div className='card-layout'>
-                <div className='card' style={{backgroundColor: 'rgba(196, 0, 0, 0.2)'}}>
-                    <div className='card-title'>40%</div>
-                    <div className='card-desc'>Fall asleep in class or can't focus due to hunger</div>
-                </div>
-                <div className='card' style={{backgroundColor: 'rgba(170, 111, 0, 0.2)'}}>
-                    <div className='card-title'>13%</div>
-                    <div className='card-desc'>Have failed assignments because they didn't have enough to eat</div>
-                </div>
-                <div className='card' style={{backgroundColor: 'rgba(179, 90, 90, 0.2)'}}>
-                    <div className='card-title'>25%</div>
-                    <div className='card-desc'>Students struggle to afford their next meal</div>
-                </div>
-            </div>
-        </section>
-        {/*How We Help*/}
-        <section className='help-background'>
-            <div className='header-layout'>
-                <h1 className='home-h1'>How We Help</h1>
-                <p className='header-description' style={{color: 'var(--gray-primary)'}}>
-                    Hearts in Action provides comprehensive support to address food and material insecurity 
-                    in our community through various initiatives.
-                </p>
-            </div>
-            <div className='card-layout'>
-                <div className='card'>
-                    <div className='icon-layout'>
-                        <div className='icon'>  
-                            <Utensils style={{color:'white', height:'60%', width: '60%'}}/>
+                <div className='card-layout'>
+                    <div className='card'>
+                        <div className='icon-layout'>
+                            <div className='icon'>  
+                                <Utensils style={{color:'white', height:'60%', width: '60%'}}/>
+                            </div>
+                            <div className='help-card-title'>Food Drives</div>
                         </div>
-                        <div className='help-card-title'>Food Drives</div>
+                        <div className='help-card-desc'>Regular food collection and distribution to students in need, ensuring no one goes hungry.</div>
                     </div>
-                    <div className='help-card-desc'>Regular food collection and distribution to students in need, ensuring no one goes hungry.</div>
-                </div>
-                <div className='card'>
-                    <div className='icon-layout'>
-                        <div className='icon'>
-                            <Users style={{color:'white', height:'60%', width: '60%'}}/>
+                    <div className='card'>
+                        <div className='icon-layout'>
+                            <div className='icon'>
+                                <Users style={{color:'white', height:'60%', width: '60%'}}/>
+                            </div>
+                            <div className='help-card-title'>Clothing Assistance</div>
                         </div>
-                        <div className='help-card-title'>Clothing Assistance</div>
+                        <div className='help-card-desc'>Warm clothing and professional attire for students to succeed academically and professionally.</div>
                     </div>
-                    <div className='help-card-desc'>Warm clothing and professional attire for students to succeed academically and professionally.</div>
-                </div>
-                <div className='card'>
-                    <div className='icon-layout'>
-                        <div className='icon'>
-                            <GraduationCap style={{color:'white', height:'60%', width: '60%'}}/>
+                    <div className='card'>
+                        <div className='icon-layout'>
+                            <div className='icon'>
+                                <GraduationCap style={{color:'white', height:'60%', width: '60%'}}/>
+                            </div>
+                            <div className='help-card-title'>Educational Support</div>
                         </div>
-                        <div className='help-card-title'>Educational Support</div>
+                        <div className='help-card-desc'>School supplies and resources for academic success, removing barriers to education.</div>
                     </div>
-                    <div className='help-card-desc'>School supplies and resources for academic success, removing barriers to education.</div>
                 </div>
-            </div>
-        </section>
-        {/*How You Can Help*/}
-        <section className='red-background' style= {{fontFamily: 'sans-serif', padding: '2rem 2rem'}}>
-            <div className='header-layout'>
-                <h1 className='home-h1'>Make a Difference</h1>
-                <p className='header-description' style={{color: 'var(--gray-primary)'}}>
-                    Every action counts. Whether you donate, volunteer, or spread awareness, you can help break the cycle of food insecurity.
-                </p>
-            </div>
-            <div style={{display: 'flex', justifyContent: 'center', margin: '0 auto 2rem', gap: '2rem'}}>
-                <NavLink className="button donate button-transition" to='/donate'>
-                        Donate
-                        <Heart style={{color:'var(--brand-secondary)', height:'1.5rem', width: '1.5rem'}}/> 
-                </NavLink>
-                <a className="button volunteer button-transition" href="https://docs.google.com/forms/d/e/1FAIpQLSd6an51-ZwG-ECVu6EZjxhaqs5CFYUOHxMBwW6c4P9KbUDc1A/viewform?fbclid=PAZXh0bgNhZW0CMTEAAafYZR3yFbKsiGBYyKinROG9mZMdK5NcPPQp-XY7oI2LvQPvq0SfG4eTXohVEw_aem_2IUtKpN5uYV4VQC8uQB82w" target="_blank" rel="noopener noreferrer">
-                        Volunteer
-                </a>
-                <NavLink className= 'button learn-more button-transition' to='/about'>
-                    Learn More
-                    <BookOpenText style={{height:'1.5rem', width: '1.5rem'}}/>
-                </NavLink>
-            </div>
-        </section>
+            </section>
+            {/*How You Can Help*/}
+            <section className='red-background' style= {{ padding: '2rem 2rem'}}>
+                <div className='header-layout'>
+                    <h1 className='home-h1'>Make a Difference</h1>
+                    <p className='header-description' style={{color: 'var(--gray-primary)'}}>
+                        Every action counts. Whether you donate, volunteer, or spread awareness, you can help break the cycle of food insecurity.
+                    </p>
+                </div>
+                <div style={{display: 'flex', justifyContent: 'center', margin: '0 auto 2rem', gap: '2rem'}}>
+                    <NavLink className="button donate button-transition" to='/donate'>
+                            Donate
+                            <Heart style={{color:'var(--brand-secondary)', height:'1.5rem', width: '1.5rem'}}/> 
+                    </NavLink>
+                    <a className="button volunteer button-transition" href="https://docs.google.com/forms/d/e/1FAIpQLSd6an51-ZwG-ECVu6EZjxhaqs5CFYUOHxMBwW6c4P9KbUDc1A/viewform?fbclid=PAZXh0bgNhZW0CMTEAAafYZR3yFbKsiGBYyKinROG9mZMdK5NcPPQp-XY7oI2LvQPvq0SfG4eTXohVEw_aem_2IUtKpN5uYV4VQC8uQB82w" target="_blank" rel="noopener noreferrer">
+                            Volunteer
+                    </a>
+                    <NavLink className= 'button learn-more button-transition' to='/about'>
+                        Learn More
+                        <BookOpenText style={{height:'1.5rem', width: '1.5rem'}}/>
+                    </NavLink>
+                </div>
+            </section>
+        </div>
         </>
     )
 }
